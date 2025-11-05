2e07e619f2b4d213bfb4371fd2c96cb1390e08c6
hide card <hidecard1500@gmail.com>
2025-11-05 15:36:49 +0630

    server

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -27,3 +28,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
